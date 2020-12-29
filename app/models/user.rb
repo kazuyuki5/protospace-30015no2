c33e551bd7dd13a9405a88207de6cbca33bb72dc
kazuyuki5 <kazuyuki2957@gmail.com>
2020-12-30 06:45:50 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validatates :name, presence: true
+  validatates :profile, presence: true
+  validatates :occupation, presence: true
+  validatates :position, presence: true
+
 end
