9e9d84c4c97c1fe7bcc2b42bdf826db642a35bdf
kazuyuki5 <kazuyuki2957@gmail.com>
2021-01-12 18:34:34 +0900

    prototypeモデルにバリデーション

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,4 +1,9 @@
 class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  validates :title, presence: ture
+  validates :catch_copy, presence: ture
+  validates :concept, presence: ture
+  validates :image, presence: ture
 end
