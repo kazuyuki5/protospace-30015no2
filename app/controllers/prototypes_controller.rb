5d47c968e330ca15f2dd5b256f2c64f090c62f83
kazuyuki5 <kazuyuki2957@gmail.com>
2021-01-16 06:56:28 +0900

    privateの追記

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,13 +8,17 @@ class PrototypesController < ApplicationController
   end
 
   def create
-    @prototype = Prototype.find(params[:prototype_id])
+    @prototype = Prototype.create(prototype_params)
     if @prototype.save
       redirect_to root_path
     else
       render :new
+    end
   end
 
   private
+
+  def prototype_params
   params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
